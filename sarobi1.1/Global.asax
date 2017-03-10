8033b579882013c370727277cfcd2fade0867361
CORP\andrey.rojas <andrey.rojas@sapiensdev.com>
2017-03-10 11:27:38 -0600

    Add project files.

---
+++ sarobi1.1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sarobi1._1.MvcApplication" Language="C#" %>
---
+++ sarobi1.1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sarobi1._1.Startup))]
+namespace sarobi1._1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
